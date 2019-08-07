ee412e75e78bba77312e18ef0db6720696bb714c
Zohaib Rauf <zabirauf@gmail.com>
2019-08-06 20:34:47 -0700

    Added docker file

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :ephemeral_share, EphemeralShareWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
